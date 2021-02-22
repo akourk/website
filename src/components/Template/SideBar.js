70aad5c7de632ca0b38c3a1702dafabaa908382e
akourk <akourk@icloud.com>
2021-02-21 23:30:40 -0800

    changing sidebar

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -13,20 +13,16 @@ const SideBar = () => (
       </Link>
       <header>
         <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+        <p><a href="mailto:akourk@icloud.com">akourk@icloud.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-        I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-        the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
-        at <a href="https://matroid.com">Matroid</a>
-        , <a href="https://planet.com">Planet</a>
-        , <a href="https://planetaryresources.com">Planetary Resources</a>
-        , <a href="https://facebook.com">Facebook</a>
-        , and <a href="https://seds.org">SEDS</a>.
+      <p>Hi, I&apos;m Alex. I like building things.
+        I am a <a href="https://www.bellevuecollege.edu/">Bellevue College</a> graduate, and
+        <a href="https://www.evergreen.edu/">a Evergreen State College</a> graduate. Before CS
+        I was a CrossFit Coach.
       </p>
       <ul className="actions">
         <li>
@@ -37,7 +33,7 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Alex Kourkoumelis <Link to="/">kourk.com</Link>.</p>
     </section>
   </section>
 );
