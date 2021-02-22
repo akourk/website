e4b0ddaf2969af6e41d05ac4aff3c3784505b39a
akourk <akourk@icloud.com>
2021-02-21 22:47:18 -0800

    fixing contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Main from '../layouts/Main';
-import EmailLink from '../components/Contact/EmailLink';
 import ContactIcons from '../components/Contact/ContactIcons';
 
 const Contact = () => (
   <Main
     title="Contact"
-    description="Contact Michael D'Angelo via email @ michael.l.dangelo@gmail.com"
+    description="Contact me via email @ akourk@icloud.com"
   >
     <article className="post" id="contact">
       <header>
@@ -17,8 +16,8 @@ const Contact = () => (
         </div>
       </header>
       <div className="email-at">
-        <p>Feel free to get in touch. You can email me at: </p>
-        <EmailLink />
+        <p>Please don&apos;t hesitate to email me at:</p>
+        <p><a href="mailto:akourk@icloud.com">akourk@icloud.com</a></p>
       </div>
       <ContactIcons />
     </article>
