543a7e9cdb2e4f6803dfdd24d26f21f43ba18145
akourk <akourk@icloud.com>
2021-02-21 23:18:16 -0800

    small updates

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,8 +5,8 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
-    + 'co-founder and CTO of Arthena, and YC Alumni.'}
+    description={"Alex Kourkoumelis's personal website. Seattle based Bellevue College graduate, "
+    + 'and aspiring Software Engineer.'}
   >
     <article className="post" id="index">
       <header>
@@ -25,7 +25,7 @@ const Index = () => (
         view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
+      <p> Source available <a href="https://github.com/akourk/website">here</a>.</p>
     </article>
   </Main>
 );
