fb0f3ff6939a50c77eb467090e8c67b36a61e166
akourk <akourk@icloud.com>
2021-07-25 14:36:14 -0700

    added a description section to jobs and updated all bullets and description.

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -4,11 +4,13 @@ const positions = [
     position: 'Abuse Detection Analyst',
     link: 'https://microsoft.com',
     daterange: 'October 2019 - Present',
+    description: 'Monitored, tracked, and analyzed instances of services abuse on Microsoft\'s platforms, providing critical feedback and input to the algorithms used to safeguard corporate systems across the world.',
     points: [
-      'Collaborate with other analysts to tune detection algorithms and identify malicious actors.',
-      'Independently derive trends in data and work with partnered teams to patch vulnerabilities.',
-      'Maintain discretion while working with highly sensitive data.',
-      'Self-managed and trusted to make difficult judgement calls even when data is limited.',
+      'Performed false-positive/false-negatives analysis, summarizing findings and equipping architects with the essential information to tune detection algorithms.',
+      'Aggregated discoveries from detailed analysis of services abuse into enhancements backlog, providing the requirements needed to incrementally improve the product.',
+      'Led team meetings to define strategy for approaching obscure cases, leading to high resolution rates.',
+      'Maintained self-driven cadence of work in an unstructured environment, leading to a high case handling rate and recognition as a high performer by the team.',
+      'Prepared and maintained comprehensive onboarding programming and responsible for training all new hires.',
     ],
   },
   {
@@ -16,11 +18,12 @@ const positions = [
     position: 'Non-Profit Volunteer Work',
     link: 'https://www.watttime.org/',
     daterange: 'September 2019 - June 2020',
+    description: 'Built automated carbon emissions data acquisition tools to form a global database to assist in time-based and geography-based "curving" of emissions to reduce carbon footprint.',
     points: [
-      'Constructed web scrapers to gather emissions data from balancing authorities world-wide.',
-      'Successfully covered all of Australia, Taiwan, and a majority of Canada.',
-      'Cleaned, converted, and stored reported data, as well as checked for inconsistencies.',
-      'Designed and implemented user-friendly frontend to visualize findings and ensure public accessibility.',
+      'Analyzed the reporting techniques of balancing authorities worldwide to determine requirements for scrapers.',
+      'Developed custom web scrapers using Python for each balancing authority globally (10+) to gather emissions data 288 times daily and post to an SQL database.',
+      'Cleaned, converted, and stored reported data, ensuring that any inconsistencies were corrected and that the end data quality was research-grade.',
+      'Built a ReactJS dashboard that leverages WattTime data to present data visually and in terms that are flexible and extensible to the end consumer.',
     ],
   },
   {
@@ -28,10 +31,11 @@ const positions = [
     position: 'Teaching Assistant',
     link: 'https://www.bellevuecollege.edu/',
     daterange: 'January 2019 - March 2019',
+    description: 'Motivated and enthusiastic Teaching Assistant for high-level CS courses, providing careful attention to students struggling with difficult concepts.',
     points: [
-      'Facilitated learning of CS topics including Theory of Computation, Context Free Grammars, Automata Theory, Finite State Machines, and Parse Trees.',
-      'Mentored students struggling with challenging problems.',
-      'Worked with professor to help innovate teaching strategies for difficult concepts.',
+      'Supplemented learning plan for struggling students in both one-on-one and in group settings.',
+      'Innovated new teaching methods by adaptively responding to student needs and influencing course curriculum.',
+      'Carefully identified different preferred learning methods to tailor specific teaching strategies on a case-by-case basis.',
     ],
   },
   {
@@ -39,9 +43,11 @@ const positions = [
     position: 'Certified Level 1 CrossFit Trainer',
     link: 'http://www.lakehillscrossfit.com/',
     daterange: 'August 2015 - May 2020',
+    description: 'Successfully aided in growing a small fitness business from start-up to profitability through innovating business strategies, fostering a welcoming community, and providing top level coaching.',
     points: [
-      'Developed and supervised client orientation while achieving 85% new client contract.',
-      'Promoted community engagement with gym members and staff resulting in 75% client retention rate and nearly 300% growth annually',
+      'Designed and developed an exhaustive, multi-step new client orientation program which resulted in an 85% new client contract rate.',
+      'Planned and organized various community engagement activities including numerous competitions, fund-raisers, and social outings, resulting in 75% client retention rate',
+      'Involved in every level of business and trusted to make major decisions bringing the business to profitability and seeing nearly 300% growth annually.',
     ],
   },
 ];
