2a84ec336f1fdd4b36feacc251ebf55c0109f60d
akourk <akourk@icloud.com>
2023-02-28 08:46:22 -0800

    dropping 14 again?

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,16 +1,10 @@
-// import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-// import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
-// import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
-// import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
-// import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
-
-const faGithub = require('@fortawesome/free-brands-svg-icons/faGithub').definition;
-const faFacebookF = require('@fortawesome/free-brands-svg-icons/faFacebookF').definition;
-const faInstagram = require('@fortawesome/free-brands-svg-icons/faInstagram').definition;
-const faLinkedinIn = require('@fortawesome/free-brands-svg-icons/faLinkedinIn').definition;
-const faEnvelope = require('@fortawesome/free-regular-svg-icons/faEnvelope').definition;
 
 const data = [
   {
